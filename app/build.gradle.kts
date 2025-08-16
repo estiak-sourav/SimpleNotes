91346739eed2fcae015ff84e212cdad8f6c2d0ee
Estiak <easourav@gmail.com>
2025-08-16 12:02:01 +0600

    update package name, and app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.simple.notes"
+        applicationId = "com.simple.notes.nno"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
